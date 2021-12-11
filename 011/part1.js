0fc778590286c698f9b939e075b66d37d932e57a
Zero Dragon <carlos@floresbenavides.com>
2021-12-11 01:19:26 -0600

    Day 11

---
+++ 011/part1.js
@@ -0,0 +1,66 @@
+const fs = require('fs')
+const { join } = require('path')
+const data = fs.readFileSync(join(__dirname, '011.txt'), 'utf8')
+
+const process = input => {
+  let cells = input
+    .split('\n')
+    .map((line) => line.trim().split('').map((cell) => ~~cell))
+    .flat()
+
+  const length = Math.sqrt(cells.length)
+
+  let step = 0
+  let totalFlashes = 0
+
+  while (step++ < 100) {
+    const flashed = new Set()
+
+    cells = cells.map((cell) => cell + 1)
+
+    const flashIndexes = cells.reduce((indexes, cell, i) => {
+      if (cell > 9) {
+        indexes.push(i)
+      }
+
+      return indexes
+    }, [])
+
+    while (flashIndexes.length) {
+      const flashIndex = flashIndexes.pop()
+
+      if (flashed.has(flashIndex)) {
+        continue
+      }
+
+      flashed.add(flashIndex);
+
+      [
+        [-1, -1], [0, -1], [1, -1],
+        [-1, 0], /* current */ [1, 0],
+        [-1, 1], [0, 1], [1, 1]
+      ].forEach(([dx, dy]) => {
+        const x = flashIndex % length + dx
+        const y = Math.floor(flashIndex / length) + dy
+
+        if (x >= 0 && x < length && y >= 0 && y < length) {
+          const index = y * length + x
+
+          cells[index] += 1
+
+          if (cells[index] > 9) {
+            flashIndexes.push(index)
+          }
+        }
+      })
+    }
+
+    cells = cells.map((cell) => cell > 9 ? 0 : cell)
+
+    totalFlashes += flashed.size
+  }
+
+  return totalFlashes
+}
+
+console.log(process(data))
